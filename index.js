5caeabb575575c549fb3636570c736b044e9b429
olufemi isarel <olufemiisraelpelumi@gmail.com>
2022-04-08 10:52:28 +0100

    Javascript Objects

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const person = {
-    name: ["Bob", "Smith"],
-    age: 32,
-    bio: function () {
-      console.log(
-        `${this.name[0]} ${this.name[1]} is ${this.age} years old.`
-      );
-    },
-    introduceSelf: function () {
-      console.log(`Hi! I'm ${this.name[0]}.`);
-    },
-  };
+  name: ['Bob', 'Smith'],
+  age: 32,
+  bio: function () {
+    console.log (`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+  },
+  introduceSelf: function () {
+    console.log (`Hi! I'm ${this.name[0]}.`);
+  },
+};
+person.age = 17;
+person['name'][0] = 'Israel';
+person['eyes'] = 'hazel';
+person.farewell = function() {
+  console.log('Bye everybody!');
+}
