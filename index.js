46868265302d70a3167e01dc0103fbe028ab6d5d
olufemi isarel <olufemiisraelpelumi@gmail.com>
2022-04-08 15:43:18 +0100

    'this' keyword

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // setting objects
 
 const person = {
-    name: ['Bob', 'Smith'],
+    name: ['Joshua', 'Timothy'],
     age: 32,
     bio: function () {
         console.log (`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
     },
-    introduceSelf: function () {
-        console.log (`Hi! I'm ${this.name[0]}.`);
+    introduction: function () {
+        console.log (`Hi! My name is ${this.name[0]}.`);
     },
 };
 
@@ -15,9 +15,9 @@ const person = {
 
 person.age = 17;
 person['name'][0] = 'Israel';
-person['eyes'] = 'hazel';
+person['color'] = 'Cyan';
 person.farewell = function() {
-    console.log('Bye everybody!');
+    console.log('Bye for now!');
 }
 
 // Let's say we wanted users to be able to store custom value types in their people data,
@@ -28,3 +28,22 @@ const myDataName = 'height';
 const myDataValue = '1.75m';
 
 person[myDataName] = myDataValue;
+
+// Using the "this" keyword
+const person1 = {
+    name: 'Zack',
+    introduction() {
+      console.log(`Hi! I'm ${this.name}.`);
+    }
+  }
+  
+  const person2 = {
+    name: 'Samuel',
+    introduction() {
+      console.log(`Hey! I'm ${this.name}.`);
+    }
+  }
+// the "this" keyword refers to the current object the code is been written in.
+
+// ############################################################################################
+
